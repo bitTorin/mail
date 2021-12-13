32d472fdbc33e8f777c7ff8c971fad1a06f361e1
bitTorin <torin.miner@gmail.com>
2021-12-13 17:17:08 -0600

    send email form complete

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -6,6 +6,8 @@ document.addEventListener('DOMContentLoaded', function() {
   document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
   document.querySelector('#compose').addEventListener('click', compose_email);
 
+  document.querySelector('#compose-form').onsubmit = send_email;
+
   // By default, load the inbox
   load_mailbox('inbox');
 });
@@ -31,3 +33,35 @@ function load_mailbox(mailbox) {
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
 }
+
+function send_email() {
+
+  const recipients = document.querySelector('#compose-recipients').value;
+  const subject = document.querySelector('#compose-subject').value;
+  const body = document.querySelector('#compose-body').value;
+  console.log(recipients);
+
+  fetch('/emails', {
+    method: 'POST',
+    body: JSON.stringify({
+        recipients: recipients,
+        subject: subject,
+        body: body
+    })
+  })
+  .then(response => response.json())
+  
+  .then(result => {
+    // Print result
+    console.log(result);
+  })
+
+  // Load sent page
+  .then(load_mailbox('sent'))
+  
+  .catch(error => {
+    console.log('Error:', error);
+  });
+
+  return false;
+};
